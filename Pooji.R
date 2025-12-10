27f24cf792eccd767cc18fbd17cb9281cd455529
Pooji856 <pn25aao@herts.ac.uk6>
2025-12-10 23:58:28 +0530

    Converted categorical variables to factors

--- Pooji.R
+++ Pooji.R
@@ -23,11 +23,47 @@ colSums(is.na(student_data))
 sum(is.na(student_data))
 
 
+
 # =========================================
 # Step 2: Check Duplicate Records
 # =========================================
 
 # Count duplicate rows
 sum(duplicated(student_data))
+
+
+# =========================================
+# Step 3: Convert appropriate categorical columns to factors
+# =========================================
+
+factor_cols <- c(
+  "Student.Age",
+  "Sex",
+  "Graduated.high.school.type",
+  "Scholarship.type",
+  "Additional.work",
+  "Regular.artistic.or.sports.activity",
+  "Do.you.have.a.partner",
+  "Total.salary.if.available",
+  "Transportation.to.the.university",
+  "Accommodation.type.in.Cyprus",
+  "Mother.s.education",
+  "Father.s.education",
+  "Parental.status",
+  "Mother.s.occupation",
+  "Father.s.occupation",
+  "Reading.frequency",
+  "Reading.frequency.1",
+  "Attendance.to.the.seminars.conferences.related.to.the.department",
+  "Impact.of.your.projects.activities.on.your.success",
+  "Attendance.to.classes",
+  "Preparation.to.midterm.exams.1",
+  "Preparation.to.midterm.exams.2",
+  "Taking.notes.in.classes",
+  "Listening.in.classes",
+  "Discussion.improves.my.interest.and.success.in.the.course",
+  "Flip.classroom",
+  "COURSE.ID"
+)
 
 
