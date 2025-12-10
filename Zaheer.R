04b4230c350ff130c4b9677955f3f0d9a2093b3a
Zaheer18-crt <zm24afv@herts.ac.uk>
2025-12-11 04:59:49 +0530

    Load Clean Dataset & Initial Preview

---
+++ Zaheer.R
@@ -0,0 +1,13 @@
+# =========================================
+# (Zaheer) - Descriptive Statistics
+# Step 1: Load Clean Dataset & Initial Preview
+# =========================================
+
+student_data <- read.csv("Final_Clean_Student_Data.csv", stringsAsFactors = FALSE)
+
+# View structure of dataset
+str(student_data)
+
+# View first 6 rows
+head(student_data)
+
