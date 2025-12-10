8cdd0ce310041f47b4a7d2cd4e5594f2a4a37e80
Pooji856 <pn25aao@herts.ac.uk6>
2025-12-10 23:33:40 +0530

    Intial Dataset Loaded

---
+++ Pooji.R
@@ -0,0 +1,13 @@
+# =========================================
+# (Pooji) - Data Loading & Cleaning
+# =========================================
+
+# Load the dataset
+student_data <- read.csv("StudentsPerformance_with_headers.csv", stringsAsFactors = FALSE)
+
+# View structure
+str(student_data)
+
+# View first 6 rows
+head(student_data)
+
